934266cdf3625c1d66a0c03c0ca17b09ad868f93
DerPenz <mike2407@t-online.de>
2022-06-05 18:06:09 +0200

    moved ws authentication

--- src/Components/Chat/Chat/Chat.jsx
+++ src/Components/Chat/Chat/Chat.jsx
@@ -1,44 +1,11 @@
-import React, { useEffect } from 'react';
-import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
-import { useWebSocket } from 'react-use-websocket/dist/lib/use-websocket';
-import useAuthentication from '../../../Util/UseAuth';
-import { WEBSOCKET_URL } from '../../../Util/Websocket';
+import React from 'react';
+
 import ChatInput from './ChatInput';
 import MessageScreen from './MessageScreen';
 import './Css/Chat.css';
 
 export default function Chat() {
-	const history = useHistory();
-
-	const { sendJsonMessage, lastJsonMessage } = useWebSocket(WEBSOCKET_URL, { share: true });
-
-	useEffect(() => {
-		const [key, , isLogedIn] = useAuthentication();
-		if (!isLogedIn) {
-			console.log('is not loged in');
-			// history.push('/authentication');
-			// return;
-		}
-		console.log('is loged in');
-
-		sendJsonMessage({ auth: key });
-
-		document.title = 'Monity | Chat';	
-	}, []);
-
-	useEffect(() => {
-		if(lastJsonMessage === null) return;
-
-		if(lastJsonMessage.error){
-			console.log("doesn't has the permission");
-			history.push('/authentication');
-			return;
-		}
-
-		console.log(lastJsonMessage);
-
-	}, [lastJsonMessage])
-
+	
 	return (
 		<div className="chat">
 			<MessageScreen />
--- src/Components/Chat/Messenger.jsx
+++ src/Components/Chat/Messenger.jsx
@@ -1,13 +1,50 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Sidebar from './Sidebar/Sidebar';
 import Chat from './Chat/Chat';
+import StatusBar from './StatusBar/StatusBar';
 import './Css/Messenger.css';
+import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
+import { useWebSocket } from 'react-use-websocket/dist/lib/use-websocket';
+import useAuthentication from '../../Util/UseAuth';
+import { WEBSOCKET_URL } from '../../Util/Websocket';
 
 export default function Messenger() {
+	const history = useHistory();
+
+	const { sendJsonMessage, lastJsonMessage } = useWebSocket(WEBSOCKET_URL, {
+		share: true,
+	});
+
+	useEffect(() => {
+		const [key, , isLogedIn] = useAuthentication();
+		if (!isLogedIn) {
+			console.log('is not loged in');
+			// history.push('/authentication');
+			// return;
+		}
+		console.log('is loged in');
+
+		sendJsonMessage({ auth: key });
+
+		document.title = 'Monity | Chat';
+	}, []);
+
+	useEffect(() => {
+		if (lastJsonMessage === null) return;
+
+		if (lastJsonMessage.error) {
+			console.log("doesn't has the permission");
+			history.push('/authentication');
+			return;
+		}
+	}, [lastJsonMessage]);
+
 	return (
 		<div className="messenger">
 			<Sidebar />
+			<div className="placeholder"></div>
 			<Chat />
+			<StatusBar />
 		</div>
 	);
 }
