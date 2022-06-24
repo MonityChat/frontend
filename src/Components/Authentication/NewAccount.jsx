bf0f83938ea0436763fb772428c2797f8549b6c2
Devin <devinfritz@gmx.de>
2022-06-24 23:12:17 +0200

    Update Success page

--- src/Components/Authentication/NewAccount.jsx
+++ src/Components/Authentication/NewAccount.jsx
@@ -1,39 +1,39 @@
-import React, { useMemo } from 'react';
-import { Link, useLocation } from 'react-router-dom';
-import './Css/NewAccount.css';
+import React, { useMemo } from "react";
+import { Link, useLocation } from "react-router-dom";
+import "./Css/NewAccount.css";
 
 /**
  * Component to display the a short information after
  * pressing on the link on the registraition email
  */
 export default function NewAccount() {
-	const query = useQuery();
+  const query = useQuery();
 
-	return (
-		<div className="inset-container new-account">
-			{query.get('error') !== 'NONE' ? (
-				<>
-					<h1 className="heading">Sorry</h1>
-					<p>An error occured during the register process</p>
-					<Link to="/login">
-						<button>Back to regiser</button>
-					</Link>
-				</>
-			) : (
-				<>
-					<h1 className="heading">Successfully</h1>
-					<p>Your Monity account was created, login to chat now</p>
-					<Link to="/login">
-						<button>Back to login</button>
-					</Link>
-				</>
-			)}
-		</div>
-	);
+  return (
+    <div className="inset-container new-account">
+      {query.get("error") !== "NONE" ? (
+        <>
+          <h1 className="heading">Sorry :(</h1>
+          <p>An error occured during the registration process</p>
+          <Link to="/login">
+            <button>Back to Login</button>
+          </Link>
+        </>
+      ) : (
+        <>
+          <h1 className="heading">Success</h1>
+          <p>Your Monity account was created, login to chat now!</p>
+          <Link to="/login">
+            <button>Back to Login</button>
+          </Link>
+        </>
+      )}
+    </div>
+  );
 }
 
 function useQuery() {
-	const { search } = useLocation();
+  const { search } = useLocation();
 
-	return useMemo(() => new URLSearchParams(search), [search]);
+  return useMemo(() => new URLSearchParams(search), [search]);
 }
