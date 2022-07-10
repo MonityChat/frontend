b41e8034dd85b4ed7659cc632de2697f142e606a
DerPenz <mike2407@t-online.de>
2022-07-10 22:03:30 +0200

    removed deprecated objects

--- src/Util/Auth.js
+++ src/Util/Auth.js
@@ -1,40 +1,5 @@
 import useAuthentication from '../Hooks/UseAuth';
 
-/**URL for getting a key
- * @deprecated
- */
-export const AUTH_KEY_URL = `${prefixDOMAIN}${DOMAIN}/auth`;
-
-/**URL for login into your account
- * @deprecated
- */
-export const LOGIN_URL = `${prefixDOMAIN}${DOMAIN}/user/login`;
-
-/**URL for registering a new account
- * @deprecated
- */
-export const REGISTER_URL = `${prefixDOMAIN}${DOMAIN}/user/register`;
-
-/**URL for getting the salt to encrypt the password
- * @deprecated
- */
-export const SALT_URL = `${prefixDOMAIN}${DOMAIN}/user/salt`;
-
-/**URL for checking if the user with a specific email or username already exists
- * @deprecated
- */
-export const USER_EXISTS_URL = `${prefixDOMAIN}${DOMAIN}/user/exists`;
-
-/**URL for requesting reseting password
- * @deprecated
- */
-export const REQEUST_RESET_PASSWORD_URL = `${prefixDOMAIN}${DOMAIN}/user/reset`;
-
-/**URL for password reset
- * @deprecated
- */
-export const RESET_PASSWORD_URL = `${prefixDOMAIN}${DOMAIN}/user/resetConfirm`;
-
 /**
  * Returns a basic session key to make requests to the server
  * @returns {String} uuid for authentication with the server
